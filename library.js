ed2643c940446048540466863029d6a013397ab9
Daniel Phillips <dan.phillips100@gmail.com>
2022-01-12 15:39:35 +0000

    Removed local storage

--- library.js
+++ library.js
@@ -5,45 +5,77 @@ document.querySelector(".date").textContent = date;
 // Create default/placeholder library data in case there is none in localStorage 
 let myLibrary = [
   {
-    name: "To Kill a Mockingbird",
+    title: "To Kill a Mockingbird",
     author: "Harper Lee",
     pages: "245",
     read: "Read"
   },
   {
-    name: "Javascript & Jquery",
+    title: "Javascript & Jquery",
     author: "Jon Duckett",
     pages: "593",
     read: "Unread"
   }
 ];
 
-//  Pull any data stored in localStorage to populate the library OR 
-//  populate the library with our own placeholder data
-const populateLibrary = () => {
-  let res = JSON.parse(localStorage.getItem('myLibrary'));
+// Iterate through myLibrary array to display book objects
+let createList = () => {
+  let elem = "<ul>";
+  myLibrary.forEach( (item, index) => {
 
-  if (res === null || res.length === 0)  {    
-    res = localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
-    window.location.reload();
-  }
+    for (let key in item) {
+      if (key != "read") {
+        elem += `<li>${item[key]}</li>`; 
+      }
+    }
 
-  return res;
+    let bookState = "";
+    if (myLibrary[index].read == "Unread") {
+      bookState = "unread"; 
+    } else { 
+      bookState = "read";
+    }
+    
+    elem += "<button class='updateRead " + bookState + "' value='" + index + "'>" + myLibrary[index].read + "</button>";
+    elem += "<button class='delete' value='" + index + "'>Delete</button></ul><ul>";
+  })
+  const page = document.querySelector(".bookList");
+  page.innerHTML = elem;
+
+  // Delete book object from array
+  document.querySelectorAll(".delete").forEach(function(item) {
+
+    item.addEventListener("click", () => {
+
+      const index = item.value;
+    
+      myLibrary.splice(index, 1);
+      createList();
+  
+    });
+  });
+  // Update read status of book object in array
+  document.querySelectorAll(".updateRead").forEach(function(item) {
+
+    item.addEventListener("click", function() {
+
+      const index = item.value;
+
+      if (myLibrary[index].read === "Read") {
+        myLibrary[index].read = "Unread";
+        createList();
+
+      } else if (myLibrary[index].read === "Unread") {
+        myLibrary[index].read = "Read";
+        createList();
+      }
+    });
+  });
 }
+ 
+createList();
 
-let library = populateLibrary();
-
-
-
-function addBookToLibrary(test) {
-  const retrievedLibrary = JSON.parse(localStorage.getItem('myLibrary')); // Parse any exisiting JSON stored in myLibrary
-  localStorage.setItem("book", JSON.stringify(test));
-  retrievedLibrary.push(test);
-  localStorage.setItem("myLibrary", JSON.stringify(retrievedLibrary));
-  window.location.reload();
-}
-
-// Function to create new book object
+// Function to create new book object 
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -51,81 +83,27 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function newBook() {
-  const name = document.querySelector("#name").value;
-  const author = document.querySelector("#author").value;
-  const pages = document.querySelector("#pages").value;
-  const status = document.querySelector("#status").value;
-  if (name != '' && author != '' && pages != '') {
-    const newAdd = new Book(name, author, pages, status); // create new object from user input
-    addBookToLibrary(newAdd) // call function to add new object to library
-    document.querySelector("#name").value = '';
-    document.querySelector("#author").value = '';
-    document.querySelector("#pages").value = '';
-    return;
-  }
-  return alert('Please dont fuck around');
-}
-
-//  Iterate through retrieved library array from 
-//  local storage and create HTML unordered list
-const createList = () => {
-  let elem = "<ul>";
-  library.forEach( (book, index) => {
-
-    for (let key in book) {
-      if (key != "read") {
-        elem += `<li>${book[key]}</li>`; 
-      }
-    }
-
-    let bookState = "";
-    if (library[index].read == "Unread") {
-      bookState = "unread"; 
-    } else { 
-      bookState = "read";
-    }
-    
-    elem += "<button class='updateRead " + bookState + "' value='" + index + "'>" + library[index].read + "</button>";
-    elem += "<button class='delete' value='" + index + "'>Delete</button></ul><ul>";
-  })
-  const page = document.querySelector(".bookList");
-  page.innerHTML = elem;
-}
-
-createList();
-
-// Delete book object from array
-  document.querySelectorAll(".delete").forEach(function(item) {
-
-    item.addEventListener("click", function() {
-
-      const index = item.value;
-      console.log(library[index]);
-      library.splice(index, 1);
-      localStorage.setItem("myLibrary", JSON.stringify(library));
-      window.location.reload();
-    });
-  });
-
-// Update read status of book object in array
-  document.querySelectorAll(".updateRead").forEach(function(item) {
-
-    item.addEventListener("click", function() {
-
-      const index = item.value;
-
-      if (library[index].read === "Read") {
-        library[index].read = "Unread";
-        localStorage.setItem("myLibrary", JSON.stringify(library));
-        window.location.reload();
-
-      } else if (library[index].read === "Unread") {
-        library[index].read = "Read";
-        localStorage.setItem("myLibrary", JSON.stringify(library));
-        window.location.reload();
+  function newBook() {
+    const name = document.querySelector("#name").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const status = document.querySelector("#status").value;
   
-      }
-    });
-  });
+    if (name != '' && author != '' && pages != '') {
+  
+      const newAddition = new Book(name, author, pages, status); // create new object from user input
 
+      myLibrary.push(newAddition);
+    
+      createList();
+  
+      document.querySelector("#name").value = '';
+      document.querySelector("#author").value = '';
+      document.querySelector("#pages").value = '';
+  
+      return;
+    }
+  
+    return alert('Form cannot be blank');
+  }
+  
