61c27115bac61e2a1f04f06ce769e51ec3d1d7c4
Dan Phillips <dan.phillips100@gmail.com>
2022-08-01 12:48:36 +0100

    feat: update read status and delete functionality refactored

--- library.js
+++ library.js
@@ -1,147 +1,144 @@
 // Create default/placeholder library data 
 let myLibrary = [
   {
-    title: "To Kill a Mockingbird",
-    author: "Harper Lee",
-    pages: "245",
-    read: "Read"
+    id: generateId(),
+    title: "React Quickly, 2nd Ed.",
+    author: "Morten Barklund & Azat Marden",
+    pages: "575",
+    read: "Read",
+    buy: "https://www.manning.com/books/react-quickly-second-edition?query=react"
   },
   {
-    title: "Javascript & Jquery",
-    author: "Jon Duckett",
-    pages: "593",
-    read: "Unread"
+    id: generateId(),
+    title: "The Joy of Javascript",
+    author: "Luis Atencio",
+    pages: "612",
+    read: "Unread",
+    buy: "https://www.manning.com/books/the-joy-of-javascript?query=the%20joy"
   },
   {
-    title: "Javascript & Jquery",
-    author: "Jon Duckett",
-    pages: "593",
-    read: "Unread"
+    id: generateId(),
+    title: "The Art of Unit Testing",
+    author: "Roy Osherove",
+    pages: "325",
+    read: "Read",
+    buy: "https://www.manning.com/books/the-art-of-unit-testing-third-edition?query=the%20art"
   },
   {
-    title: "Javascript & Jquery",
-    author: "Jon Duckett",
-    pages: "593",
-    read: "Unread"
+    id: generateId(),
+    title: "ASP.NET Core Security",
+    author: "Christian Wenz",
+    pages: "368",
+    read: "Unread",
+    buy: "https://www.manning.com/books/asp-net-core-security?query=asp"
   },
   {
-    title: "Javascript & Jquery",
-    author: "Jon Duckett",
-    pages: "593",
-    read: "Unread"
+    id: generateId(),
+    title: "TypeScript Quickly",
+    author: "Yakov Fain & Anoton Moiseev",
+    pages: "488",
+    read: "Unread",
+    buy: "https://www.manning.com/books/typescript-quickly?query=type"
   }
 ]
+
+createList()
 class bookClass {
-    constructor(title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
+  constructor(title, author, pages, read) {
+    this.id = generateId()
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
 }    
 
-  function newBook() {
-      const name = document.querySelector("#name").value;
-      const author = document.querySelector("#author").value;
-      const pages = document.querySelector("#pages").value;
-      const status = document.querySelector("#status").value;
+function newBook() {
+  const name = document.querySelector("#name").value;
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  const status = document.querySelector("#status").value;
     
-      if (name != '' && author != '' && pages != '') {
-    
-        const newAddition = new bookClass(name, author, pages, status); // create new object from user input
-  
-        myLibrary.push(newAddition);
-        modal.style.display = 'none'
-        const bookListWrapper = document.getElementById('bookList')
-        bookListWrapper.innerHTML = ''
-        createList();
-    
-        document.querySelector("#name").value = '';
-        document.querySelector("#author").value = '';
-        document.querySelector("#pages").value = '';
-    
-        return;
-      }
-    
-      return alert('Form cannot be blank');
-    }
+  if (name != '' && author != '' && pages != '') {
+    const newAddition = new bookClass(name, author, pages, status); // create new object from user input
+    myLibrary.push(newAddition);
+    modal.style.display = 'none'
+    createList();
+    document.querySelector("#name").value = '';
+    document.querySelector("#author").value = '';
+    document.querySelector("#pages").value = '';
+    return;
+  }
+  return alert('Form cannot be blank');
+}
 
-// Iterate through myLibrary array to display book objects
-// const createList = () => {
-//   let elem = "<ul>";
-//   myLibrary.forEach( (item, index) => {
-
-//     for (let key in item) {
-//       if (key != "read") {
-//         elem += `<li>${item[key]}</li>`; 
-//       }
-//     }
-
-//     let bookState = "";
-//     if (myLibrary[index].read == "Unread") {
-//       bookState = "unread"; 
-//     } else { 
-//       bookState = "read";
-//     }
-    
-//     elem += "<button class='updateRead " + bookState + "' value='" + index + "'>" + myLibrary[index].read + "</button>";
-//     elem += "<button class='delete' value='" + index + "'>Delete</button></ul><ul>";
-//   })
-//   const page = document.querySelector(".bookList");
-//   page.innerHTML = elem;
-
-//   // Delete book object from array
-//   document.querySelectorAll(".delete").forEach(function(item) {
-
-//     item.addEventListener("click", () => {
-
-//       const index = item.value;
-    
-//       myLibrary.splice(index, 1);
-//       createList();
-  
-//     });
-//   });
-
-//   // Update read status of book object in array
-//   document.querySelectorAll(".updateRead").forEach(function(item) {
-
-//     item.addEventListener("click", function() {
-
-//       const index = item.value;
-
-//       if (myLibrary[index].read === "Read") {
-//         myLibrary[index].read = "Unread";
-//         createList();
-
-//       } else if (myLibrary[index].read === "Unread") {
-//         myLibrary[index].read = "Read";
-//         createList();
-//       }
-//     });
-//   });
-// }
+function generateId() {
+  return Math.random() * 10000
+}
 
 function createList() {
+  
   const bookListWrapper = document.getElementById('bookList')
+  bookListWrapper.innerHTML = ''
   
   myLibrary.forEach(book => {
     const container = document.createElement('div')
     container.className = 'bookCard'
+
     const title = document.createElement('h1')
     title.innerText = book.title
+
     const author = document.createElement('h3')
     author.innerText = `Written by ${book.author}`
+
     const pages = document.createElement('p')
     pages.innerText = `${book.pages} pages`
-    container.append(title, author, pages)
-    bookListWrapper.append(container)
-  })
-  
-}
- 
-createList();
+    
+    const readButton = document.createElement('button')
+    const status = readStatus(`${book.read}`)
+    readButton.type = 'submit'
+    readButton.className = 'readStatus'
+    readButton.id = book.id
+    readButton.innerText = status
+    readButton.addEventListener('click', updateReadStatus)
 
+
+    const deleteButton = document.createElement('button')
+    deleteButton.type = 'submit'
+    deleteButton.id = book.id
+    deleteButton.className = 'deleteBook'
+    deleteButton.innerText = 'Delete'
+    deleteButton.addEventListener('click', deleteBook)
+
+    container.append(title, author, pages, readButton, deleteButton)
+    bookListWrapper.append(container)
+
+  })
+}
+
+function readStatus(arg) {
+  if (arg === 'Read') {
+    return 'Read'
+  } else {
+    return 'Unread'
+  }
+}
+
+function updateReadStatus(event) {
+  const index = myLibrary.findIndex(object => object.id === Number(event.target.id) )
+  if (myLibrary[index].read === 'Read') {
+    myLibrary[index].read = 'Unread'
+  } else {
+    myLibrary[index].read = 'Read'
+  }
+  return createList()
+}
+
+function deleteBook(event) {
+  const index = myLibrary.findIndex(object => object.id === Number(event.target.id) )
+  myLibrary.splice(index, 1)
+  createList()
+}
 
 // Modal 
 const modal = document.getElementById('modal')
